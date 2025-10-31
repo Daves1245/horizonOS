8101eba54719f9e3384511d2d9a858048be19431
David Santamaria <davidsantamaria1245@gmail.com>
2025-10-31 06:52:35 -0400

    add helper library files for char types

---
+++ kernel/arch/i386/common/ctype.h
@@ -0,0 +1,11 @@
+#ifndef CTYPE_H
+#define CTYPE_H
+
+// Character classification functions
+int isprintable(char c);
+int iswhitespace(char c);
+int isalpha(char c);
+int isnum(char c);
+int isalphanum(char c);
+
+#endif
