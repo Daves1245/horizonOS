980e5e5dc1b9b7cc54557b25d68349d8e6b67980
David Santamaria <davidsantamaria1245@gmail.com>
2025-11-01 16:15:49 -0400

    expose terminal scrolln

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -4,6 +4,7 @@
 #include "common.h"
 #include <string.h>
 #include <stdint.h>
+#include "common/logger.h"
 
 void terminal_initialize(void);
 void terminal_putchar(char c);
@@ -11,12 +12,10 @@ void terminal_write(const char *data, size_t size);
 void terminal_write_dec(uint32_t n);
 void terminal_writestring(const char *data);
 void terminal_scroll();
+void terminal_scrolln();
 
-// Color printing helpers
 void terminal_setcolor(uint8_t color);
 void print_colored(const char* str, uint8_t fg, uint8_t bg);
 
-// Logger functions (from i386-specific logger.h)
-#include "common/logger.h"
 
 #endif
