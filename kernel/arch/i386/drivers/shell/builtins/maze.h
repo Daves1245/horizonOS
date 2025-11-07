a78557d2defc89000a3b5f8c140cf6a72bf2281e
David Santamaria <davidsantamaria1245@gmail.com>
2025-11-06 23:29:57 -0500

    straightforward backtracker maze generation header

--- kernel/arch/i386/drivers/shell/builtins/maze.h
+++ kernel/arch/i386/drivers/shell/builtins/maze.h
@@ -1,6 +1,8 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+#include <stdint.h>
+
 #define WALL 0
 #define CORRIDOR 1
 #define VISITED 2
@@ -8,19 +10,32 @@
 #define SOURCE 4
 #define SINK 5
 
+#define MAZE_WIDTH 39
+#define MAZE_HEIGHT 10
+#define DISPLAY_HEIGHT 23
+#define DISPLAY_WIDTH 80
+
 struct cell {
-  int top_wall;
-  int bottom_wall;
+  int type;
+  // bools - has wall
   int right_wall;
-  int left_wall;
+  int bottom_wall;
 };
 
-// we leave one row available for drawing text
-// this will be hints for what keys to press to generate a maze, solve it, etc.
-struct cell grid[24][80];
+extern struct cell maze[MAZE_HEIGHT][MAZE_WIDTH];
+extern char display_buffer[DISPLAY_HEIGHT][DISPLAY_WIDTH];
+extern int next_set_id;
 
-void draw();
+void seed_rng(uint32_t seed);
+uint32_t random_next();
+uint32_t random_range(uint32_t max);
+
+void maze_init();
+void draw_maze();
+void display_maze();
 void generate_maze();
 void solve_maze();
+void draw_help_text();
+void clear_display_buffer();
 
 #endif
