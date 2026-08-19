5cc4fe19f130032ea6361bd1a0b500aab6cc450d
David Santamaria <dsantamaria1245@gmail.com>
2026-08-18 23:50:02 -0400

    x86_64/scheduler: prevent process starvation
    - guard with nested irq restoration

--- kernel/arch/x86_64/scheduler/scheduler.c
+++ kernel/arch/x86_64/scheduler/scheduler.c
@@ -8,10 +8,7 @@ extern void swtch(uint64_t *old_rsp, uint64_t new_rsp, uint64_t new_cr3);
 void ctx_switch(struct process *new) {
     // TODO(multicore)
 
-    // push registers onto stack, reset rsp, and pop
-    // new registers
-
-    disable_interrupts();
+    uint64_t flags = irq_save();
 
     struct process *old = myproc();
 
@@ -19,14 +16,17 @@ void ctx_switch(struct process *new) {
 
     swtch(&old->context.rsp, new->context.rsp, new->context.cr3);
 
-    enable_interrupts();
+    irq_restore(flags);
 }
 
 void scheduler() {
-    struct process *p;
+    struct process *p = &processes[0];
 
     while (1) {
         /* avoid deadlock if all other processes are waiting */
+        // this is safe since once we're in the scheduler
+        // we're guaranteed that IF is 1 for all processes. we
+        // don't switch out while we're in a critical section.
         asm volatile("cli");
         asm volatile("sti");
 
@@ -38,16 +38,19 @@ void scheduler() {
         // each cpu must run its own scheduler(). for a multicore system,
         // we must wait at the end for an interrupt at the end from another
         // core.
-        for (p = &processes[0]; p < &processes[NUM_PROCESSES]; p++) {
+        for (; !found ;) {
             if (p->state == READY) {
                 p->state = RUNNING;
                 ctx_switch(p);
+                if (p->state == RUNNING) {
+                    p->state = READY;
+                }
                 found = 1;
             }
-        }
 
-        if (!found) {
-            // asm volatile ("hlt");
+            if (p == &processes[NUM_PROCESSES]) {
+                p = &processes[0];
+            }
         }
     }
 }
