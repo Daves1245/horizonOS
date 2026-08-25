9e56293e1a56b965777f9f3ebeb548870cc7cc2f
David Santamaria <dsantamaria1245@gmail.com>
2026-08-25 16:04:25 -0400

    x86_64/timer: prepare for reentrant scheduler
    - add second scheduler-compatible ISR for timer interrupt
    - add TIMER_INT_NO macro
    - add timer.h include to timer.c
    - add missing function declarations in timer.h

--- kernel/kernel/drivers/timer.c
+++ kernel/kernel/drivers/timer.c
@@ -1,8 +1,15 @@
 #include <apic/apic.h>
 #include <interrupts/isr.h>
+#include <kernel/panic.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <kernel/tty.h>
+#include <drivers/timer.h>
+
+#ifdef __x86_64__
+#include <x86_64/scheduler/process.h>
+#include <x86_64/scheduler/scheduler.h>
+#endif
 
 #define TIMER_HZ 1000
 
@@ -24,18 +31,29 @@ void timer_interrupt_handler(struct interrupt_context *regs) {
     apic_send_eoi();
 }
 
+#ifdef __x86_64__
+void timer_interrupt_handler_sched(struct interrupt_context *regs) {
+  tick_count++;
+  apic_send_eoi();
+  if (mycpu() == NULL) {
+    panic("[timer]: mycpu() returned NULL");
+  }
+  ctx_switch(mycpu()->scheduler_proc);
+}
+#endif
+
 void init_timer() {
-    log_debug("[timer]: registering timer handler for vector 32\n");
-    register_interrupt_handler(32, timer_interrupt_handler);
-    apic_timer_init(TIMER_HZ);
-    log_success("[timer]: APIC timer initialized at %d Hz\n", TIMER_HZ);
+  log_debug("[timer]: registering timer handler for vector 32\n");
+  register_interrupt_handler(TIMER_INT_NO, timer_interrupt_handler);
+  apic_timer_init(TIMER_HZ);
+  log_success("[timer]: APIC timer initialized at %d Hz\n", TIMER_HZ);
 }
 
 void sleep_ms(uint32_t ms) {
-    uint32_t start = tick_count;
-    while ((tick_count - start) < ms);
+  uint32_t start = tick_count;
+  while ((tick_count - start) < ms);
 }
 
 void sleep(uint32_t s) {
-    sleep_ms(s * 1000);
+  sleep_ms(s * 1000);
 }
--- kernel/kernel/drivers/timer.h
+++ kernel/kernel/drivers/timer.h
@@ -2,6 +2,7 @@
 #define TIMER_H
 
 #include <stdint.h>
+#include <interrupts/isr.h>
 
 /* polls condition until true or ms elapses. returns 1 if condition met, 0 if timed out */
 #define timeout(ms, condition) ({                         \
@@ -11,9 +12,15 @@
     (condition);                                          \
 })
 
+#define TIMER_INT_NO 32
+
 void init_timer();
 uint32_t timer_ticks();
 void sleep(uint32_t s);
 void sleep_ms(uint32_t ms);
+
+/* ISRs */
+void timer_interrupt_handler(struct interrupt_context *regs);
+void timer_interrupt_handler_sched(struct interrupt_context *regs);
 
 #endif
