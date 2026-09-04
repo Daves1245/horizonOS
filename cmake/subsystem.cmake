a72a9438d199d849c1d7b33baf68215649fe1adc
David Santamaria <dsantamaria1245@gmail.com>
2026-09-03 21:00:50 -0400

    CMakeLists.txt: use one subsystem cmake function

--- cmake/subsystem.cmake
+++ cmake/subsystem.cmake
@@ -1,5 +1,5 @@
-function(mm name)
-    cmake_parse_arguments(S "" "" "CORE;ASM;DEPENDS;INCLUDES;DEFINES" ${ARGN})
+function(subsystem name)
+    cmake_parse_arguments(S "" "" "CORE;INCLUDES" ${ARGN})
 
     if (S_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "subsystem ${name}: stray arguments (mispelled arguments?): ${S_UNPARSED_ARGUMENTS}")
@@ -14,21 +14,12 @@ function(mm name)
     endif()
 
     # TODO split between i386 and x86_64
-
-    set(MM_SOURCES
-        mm/kheap.c
-        mm/mm.c
-    )
-
     add_library(${name} OBJECT ${S_CORE} ${S_ASM})
 
     # horizon_flags carries common flags; 'depends' gives the subsystem
     # the includes of other subsystems (include/kernel and such)
     target_link_libraries(${name} PRIVATE horizon_flags ${S_DEPENDS})
-
-    target_include_directories(${name}
-        PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include
-        PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}
-    )
+    target_include_directories(${name} PUBLIC ${S_INCLUDES})
     target_compile_definitions(${name} PRIVATE ${S_DEFINES})
+    target_link_libraries(horizon.kernel PRIVATE ${name})
 endfunction()
